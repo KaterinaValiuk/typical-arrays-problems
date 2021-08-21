5315bd9f127280b200aa000fd3018ac7d268b3c9
KaterinaValiuk <e5054404@gmail.com>
2021-08-21 22:38:54 +0300

    docs: the task was successfully completed

--- src/index.js
+++ src/index.js
@@ -1,29 +1,40 @@
 /*jshint esversion: 8 */
 exports.min = function min (array) {
   let minNum = 0;
-  for (i = 0; i < array.length; ++i) {
-    if (array[i] < minNum) {minNum = array[i];}
-  }
+  if (!array) {
+    minNum = 0;
+  } else {
+  for (i = 1; i <= array.length; ++i) {
+    if (array[i - 1] < minNum) {minNum = array[i - 1];}
+  }}
   return minNum;
 };
 
 exports.max = function max (array) {
   let maxNum = 0;
-  for (i = 0; i < array.length; ++i) {
-    if (array[i] > maxNum) {maxNum = array[i];}
-  }
+  if (!array) {
+    maxNum = 0;
+  } else {
+  for (i = 1; i <= array.length; ++i) {
+    if (array[i - 1] > maxNum) {maxNum = array[i - 1];}
+  }}
   return maxNum;
 };
 
 exports.avg = function avg (array) {
   let sum = 0;
-  //Array.isArray(array) ? sum = 0 : (
+  let avgNum = 0;
+  if (!array) {
+    avgNum = 0;
+  } else {
 
   for (i = 0; i < array.length; ++i) {
     sum = sum + array[i];}
   //  );
-
-  let avgNum = sum / array.length;
+  
+  if (!array.length || array.length === 0) {
+    avgNum = 0;
+  } else {avgNum = sum / +(array.length);}}
   return avgNum;
     
 };
