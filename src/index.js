90df965e7bc2b587b4622f4526073d6d7ffae16e
KaterinaValiuk <e5054404@gmail.com>
2021-08-21 22:10:58 +0300

    docs: started to complete the task, all problems are not solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-
+/*jshint esversion: 8 */
 exports.min = function min (array) {
-  return 0;
-}
+  let minNum = 0;
+  for (i = 0; i < array.length; ++i) {
+    if (array[i] < minNum) {minNum = array[i];}
+  }
+  return minNum;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  let maxNum = 0;
+  for (i = 0; i < array.length; ++i) {
+    if (array[i] > maxNum) {maxNum = array[i];}
+  }
+  return maxNum;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  let sum = 0;
+  //Array.isArray(array) ? sum = 0 : (
+
+  for (i = 0; i < array.length; ++i) {
+    sum = sum + array[i];}
+  //  );
+
+  let avgNum = sum / array.length;
+  return avgNum;
+    
+};
